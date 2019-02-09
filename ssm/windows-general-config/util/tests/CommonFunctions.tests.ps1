f170d1978aec7296743790809e7e8725f35f8a27
Brian Olson <bolson@docutap.us>
2019-02-09 14:22:06 -0600

    Updating SSM association targets

--- ssm/windows-general-config/util/tests/CommonFunctions.tests.ps1
+++ ssm/windows-general-config/util/tests/CommonFunctions.tests.ps1
@@ -1,7 +1,7 @@
 Import-Module .\CommonFunctions.psm1;
 
-# Test all of the citrix common functions
-Describe "Get Instance ID" {
+# Tests the get instance ID function
+Describe "Get Instance ID Tests" {
     InModuleScope CommonFunctions {
         It "Calls meta data to get instance id" {
             Mock Invoke-RestMethod {return "instance_name"} -parameterfilter {$Uri -eq "http://169.254.169.254/latest/meta-data/instance-id"} -Verifiable -modulename CommonFunctions;
@@ -12,6 +12,18 @@ Describe "Get Instance ID" {
         It "Returns the meta data result for the instance name" {
             Mock Invoke-RestMethod {return "instance_name"} -modulename CommonFunctions -Verifiable;
             Get-MyInstanceId | Should Be "instance_name"
+        }
+    }
+}
+
+# Tests the get instance ID function
+Describe "Bomb Function Tests" {
+    InModuleScope CommonFunctions {
+        It "Sets the error code passed in" {
+            Mock Test-Bomb {}  -Verifiable -modulename CommonFunctions;
+            Invoke-Bomb "Something went wrong" 9;
+            $global:exitCode | Should Be 9;
+            Assert-VerifiableMocks;
         }
     }
 }
